a155194105bd33effe1d18fc17061e45d20a4244
Gulzar Arif <gulzaararif173@gmail.com>
2026-08-06 07:59:36 -0700

    Add Google Search Console verification file

--- src/app/google79cde6fe91873109.html/route.ts
+++ src/app/google79cde6fe91873109.html/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return new NextResponse("google79cde6fe91873109", {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
+  return new NextResponse(
+    "google79cde6fe91873109.html",
+    {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }
+  );
 }
