5bb01896d2099f4c5da52e269a7e84ff1a72fc5a
Gulzar Arif <gulzaararif173@gmail.com>
2026-08-07 03:14:35 -0700

    Add Google verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="5SfN9xWpXpx-AbxA78gQcNVbMZXMi3cOJFeb8e664Ds"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
